54e2c30a087e4d1ce688b82086741bb80ff91b02
bokaiii <jakob1303@hotmail.com>
2024-06-17 19:20:52 +0200

    Added footer + fixed full-width issues

--- src/app/portfolio-overview/portfolio-overview.component.ts
+++ src/app/portfolio-overview/portfolio-overview.component.ts
@@ -3,11 +3,12 @@ import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { SectionHomeComponent } from '../section-home/section-home.component';
 import { SectionAboutComponent } from '../section-about/section-about.component';
 import { SectionSkillsComponent } from '../section-skills/section-skills.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-portfolio-overview',
   standalone: true,
-  imports: [NavBarComponent, SectionHomeComponent, SectionAboutComponent, SectionSkillsComponent],
+  imports: [NavBarComponent, SectionHomeComponent, SectionAboutComponent, SectionSkillsComponent, FooterComponent],
   templateUrl: './portfolio-overview.component.html',
   styleUrl: './portfolio-overview.component.scss'
 })
