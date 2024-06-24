ae1ba10cb560d98f4eeb93809246cfcf417f4147
bokaiii <jakob1303@hotmail.com>
2024-06-24 07:43:33 +0200

    Deleted project on branch PROD

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { PortfolioOverviewComponent } from './portfolio-overview/portfolio-overview.component';
-
-export const routes: Routes = [
-    {
-        path: '',
-        component: PortfolioOverviewComponent
-    }
-];
